4667e2f55616103c39d4a05f26951512b18ee726
ALenaLesnik <90832762+ALenaLesnik@users.noreply.github.com>
2021-10-05 14:27:38 +0300

    Update and rename index.html to index.py

---
+++ index.py
@@ -0,0 +1,9 @@
+# Импортируем подмодули Flask для запуска веб-сервиса
+from flask import Flask, request, redirect, session, url_for
+
+app = Flask(__name__)
+
+@app.route("/"])
+def main():
+# Функция получает тело запроса и возвращает ответ
+    return "Hello World"
