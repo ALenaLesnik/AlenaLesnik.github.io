171a076c48766bc2451c73836d74aead6d0cafa8
ALenaLesnik <90832762+ALenaLesnik@users.noreply.github.com>
2021-10-05 14:34:40 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,18 @@
 # Импортируем подмодули Flask для запуска веб-сервиса
 from flask import Flask, request, redirect, session, url_for
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST'])
 def main():
 # Функция получает тело запроса и возвращает ответ
-    return "Hello from github"
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        "response": {
+            "end_session": False
+        }
+    }
+    response['response']['text'] = 'Все говорят купи слона!'
+    return json.dumps(response)
